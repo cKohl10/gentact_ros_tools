14ec0ef26d4a5439b0cccf0c38b46d264f271579
Carson Kohlbrenner <82614880+cKohl10@users.noreply.github.com>
2025-07-17 15:37:00 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,19 @@ from setuptools import find_packages, setup
 
 package_name = 'gentact_ros_tools'
 
+# Function to recursively get all files in a directory
+def get_data_files_recursive(base_dir, target_dir):
+    files = []
+    for root, dirs, filenames in os.walk(base_dir):
+        for filename in filenames:
+            file_path = os.path.join(root, filename)
+            # Calculate the relative path from base_dir
+            rel_path = os.path.relpath(file_path, base_dir)
+            # Create the target path
+            target_path = os.path.join(target_dir, rel_path)
+            files.append((os.path.dirname(target_path), [file_path]))
+    return files
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -14,10 +27,9 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
-        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
         (os.path.join('share', package_name, 'config'), glob('config/*')),
         (os.path.join('share', package_name, 'msg'), glob('gentact_ros_tools/msg/*.msg')),
-    ],
+    ] + get_data_files_recursive('meshes', os.path.join('share', package_name, 'meshes')),
     install_requires=['setuptools', 'rclpy'],
     zip_safe=True,
     maintainer='carson',
